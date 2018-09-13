9cb01df036958cde461e500139c710e04c477ac8
Andy Giles <andyg5000@gmail.com>
2018-09-13 12:19:27 -0400

    Updates to blueoi_privacy hook_theme and template

--- modules/blueoi_privacy/templates/blueoi-privacy-sticky-footer.tpl.php
+++ modules/blueoi_privacy/templates/blueoi-privacy-sticky-footer.tpl.php
@@ -1,4 +1,4 @@
 <div id="blueoi-privacy-sticky-footer">
     <a href="#" class="blueoi-privacy-close">x</a>
-    <p>We use cookies on this site to enhance your user experience. By clicking any link on this page you are giving your consent for us to set cookies. Read our cookie policy.</p>
+    <p><?php print $cookie_message; ?></p>
 </div>
