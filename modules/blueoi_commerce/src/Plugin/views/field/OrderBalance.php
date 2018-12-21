dd1bb2552c5b0fffe3a5cc9fca0cbc07bde568a3
Andy Giles <andyg5000@gmail.com>
2018-12-21 15:53:06 -0500

    Added order balance field to views

---
+++ modules/blueoi_commerce/src/Plugin/views/field/OrderBalance.php
@@ -0,0 +1,40 @@
+<?php
+namespace Drupal\blueoi_commerce\Plugin\views\field;
+
+use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler for order balance.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("blueoi_commerce_order_balance")
+ */
+class OrderBalance extends FieldPluginBase {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $balance = '';
+    $order = $values->_entity;
+    if (!empty($order) && $order instanceof OrderInterface) {
+      $order_balance = $order->getBalance();
+      if (!empty($order_balance)) {
+        $currency_formatter = \Drupal::service('commerce_price.currency_formatter');
+        $balance = $currency_formatter->format((string)$order_balance->getNumber(), $order_balance->getCurrencyCode());
+      }
+    }
+    return $balance;
+  }
+  
+}
