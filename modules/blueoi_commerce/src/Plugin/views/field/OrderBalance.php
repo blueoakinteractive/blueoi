0618057330463ac2ddc673b1a4bea32586691536
Andy Giles <andyg5000@gmail.com>
2019-09-23 10:13:22 -0400

    Manually invoke the order updater service before displaying the balance

--- modules/blueoi_commerce/src/Plugin/views/field/OrderBalance.php
+++ modules/blueoi_commerce/src/Plugin/views/field/OrderBalance.php
@@ -2,6 +2,7 @@
 namespace Drupal\blueoi_commerce\Plugin\views\field;
 
 use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\commerce_payment\PaymentOrderUpdaterInterface;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
 
@@ -28,6 +29,10 @@ class OrderBalance extends FieldPluginBase {
     $balance = '';
     $order = $values->_entity;
     if (!empty($order) && $order instanceof OrderInterface) {
+      /** @var PaymentOrderUpdaterInterface $order_updater */
+      $payment_updater = \Drupal::service('commerce_payment.order_updater');
+      $payment_updater->updateOrder($order);
+
       $order_balance = $order->getBalance();
       if (!empty($order_balance)) {
         $currency_formatter = \Drupal::service('commerce_price.currency_formatter');
@@ -36,5 +41,5 @@ class OrderBalance extends FieldPluginBase {
     }
     return $balance;
   }
-  
+
 }
